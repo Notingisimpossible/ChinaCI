1039898c1d82b0097eb0981b519ead8917dfacf3
notingisimpossible <2421665649@qq.com>
2020-08-28 22:46:47 +0800

    抽象sql

--- JJXYServer/abstractSql/index.js
+++ JJXYServer/abstractSql/index.js
@@ -1,8 +1,17 @@
-const findSql = (value) => {
-  let sql = `select * from  XYC_V_TJX_CJXX where id='${value.id}'`
+const findCJSql = (value) => {
+  let sql = value.time ? `select * from  XYC_V_TJX_CJXX where id='${value.id}' and XQ='${value.time}'`:`select * from  XYC_V_TJX_CJXX where id='${value.id}'`
   return sql
 }
-
+const findKCBSql = (value) => {
+  let sql = '****'
+  return sql
+}
+const findCardSql = (value) => {
+  let sql = '***'
+  return sql
+}
 module.exports = {
-  findSql
+  findCJSql,
+  findKCBSql,
+  findCardSql
 } 
