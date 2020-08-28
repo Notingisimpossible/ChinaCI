781624051eaa3be3175a16522e99e90f1e862da5
notingisimpossible <2421665649@qq.com>
2020-08-28 22:49:21 +0800

    假数据

---
+++ JJXYServer/mock/data.js
@@ -0,0 +1,176 @@
+const studentCardInfo = {
+  money: 150,
+  num: "7741",
+  status: "正常",
+  type: "1类卡"
+}
+const takeMoneyInfo = [
+  {
+    id: 1,
+    time:"2019/11/8 18:28:55",
+    address:"一食堂123",
+    descript: "餐费支出",
+    money:"-10"
+  },
+  {
+    id: 2,
+    time:"2019/11/8 18:28:55",
+    address:"一食堂113",
+    descript: "餐费支出",
+    money:"-12"
+  },
+  {
+    id: 3,
+    time:"2019/11/8 18:22:55",
+    address:"一食堂133",
+    descript: "餐费支出",
+    money:"-10"
+  },
+  {
+    id: 4,
+    time:"2019/11/11 18:28:55",
+    address:"一食堂23",
+    descript: "餐费支出",
+    money:"-20"
+  },
+  {
+    id: 5,
+    time:"2019/11/12 18:28:55",
+    address:"一食堂12",
+    descript: "餐费支出",
+    money:"-7"
+  }
+]
+const classInfo = [
+  {
+    id: 1,
+    teacher: "林治",
+    className:"外科护理学II",
+    time:"1-16周",
+    classTime: "[01-02节]",
+    data: "星期一",
+    address: "基护景教室(3)"
+  },
+  {
+    id: 2,
+    teacher: "习徽娜",
+    className:"护理研究",
+    time:"1-16周",
+    classTime: "[01-02节]",
+    data: "星期二",
+    address: "主A212^"
+  },
+  {
+    id: 3,
+    teacher: "林治",
+    className:"外科护理学II",
+    time:"1-16周",
+    classTime: "[01-02节]",
+    data: "星期三",
+    address: "基护景教室(2)"
+  },
+  {
+    id: 4,
+    teacher: "黄玉风",
+    className:"内科护理学II",
+    time:"1-16周",
+    classTime: "[01-02-03节]",
+    data: "星期四",
+    address: "基护示教室(1)"
+  },
+  {
+    id: 5,
+    teacher: "林治",
+    className:"外科护理学II",
+    time:"1-16周",
+    classTime: "[01-02节]",
+    data: "星期五",
+    address: "基护示教室(2)"
+  },
+  {
+    id: 6,
+    teacher: "习徽娜",
+    className:"护理研究",
+    time:"1-16周",
+    classTime: "[03-04节]",
+    data: "星期三",
+    address: "主B309^"
+  },
+  {
+    id: 7,
+    teacher: "习徽娜",
+    className:"护理研究",
+    time:"1-16周",
+    classTime: "[03-04节]",
+    data: "星期五",
+    address: "主B309^"
+  },
+  {
+    id: 8,
+    teacher: "贺春英",
+    className:"护理管理学",
+    time:"1-5周",
+    classTime: "[05-06-07节]",
+    data: "星期三",
+    address: "基护教室(3)"
+  },
+  {
+    id: 9,
+    teacher: "",
+    className:"急重症护理学",
+    time:"1-16周",
+    classTime: "[05-06节]",
+    data: "星期四",
+    address: "主D5多媒体20"
+  },
+  {
+    id: 9,
+    teacher: "",
+    className:"急重症护理学",
+    time:"1-16周",
+    classTime: "[05-06节]",
+    data: "星期五",
+    address: "主D5多媒体19"
+  },
+  {
+    id: 10,
+    teacher: "",
+    className:"急重症护理学",
+    time:"1-16周",
+    classTime: "[07-08节]",
+    data: "星期一",
+    address: "主D5多媒体19"
+  },
+  {
+    id: 11,
+    teacher: "贺春英",
+    className:"护理管理学",
+    time:"1-5周",
+    classTime: "[05-06-07节]",
+    data: "星期三",
+    address: "基护教室(3)"
+  },
+  {
+    id: 12,
+    teacher: "邬贤臣",
+    className:"形势与政策VI",
+    time:"1-5周",
+    classTime: "[07-08节]",
+    data: "星期四",
+    address: "主附西多媒体4"
+  },
+  {
+    id: 13,
+    teacher: "贺春英",
+    className:"护理管理学",
+    time:"1-6周",
+    classTime: "[07-08节]",
+    data: "星期五",
+    address: "基护教室(2)"
+  }
+]
+module.exports = {
+  studentCardInfo,
+  takeMoneyInfo,
+  classInfo
+}
